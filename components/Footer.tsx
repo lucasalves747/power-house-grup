80c269bc5d625409c810965be474806c1f46563a
lucas2651 <134lucasalves@gmail.com>
2025-12-09 12:08:56 -0300

    first

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
         <img 
   src="https://storage.googleapis.com/msgsndr/dkM0aNpySiIFf3uusFTa/media/6938351b10381826274a643d.png"
   alt="Power House"
-  className="h-8 grayscale opacity-50 hover:opacity-100 transition-all rotate-90"
+  className="h-40 grayscale opacity-50 hover:opacity-100 transition-all rotate-90"
 />
         <div className="text-gray-600 text-sm text-center md:text-right">
           <p>© 2025 Power House Miami. Todos os direitos reservados.</p>
